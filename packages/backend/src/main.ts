dde07a0e9d13319a3dd2f092194d86d89b623f04
ayushmi173 <ayushmishra173@gmail.com>
2021-09-19 23:43:59 +0530

    websockets creating messages

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 4000;
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
+  await app.listen(port);
+  console.log(`listening on port ${port}`);
 }
 bootstrap();
--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Socket, io } from 'socket.io-client';
 
-function App() {
+const App: React.FC = () => {
+  const socket = io(`http://${window.location.hostname}:4000`, {
+    host: '4000',
+    transports: ['websocket'],
+  });
+
+  const [message, setMessage] = useState<string>('');
+  const [entireMessages, setEntireMessages] = useState<Socket[]>([]);
+
+  const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const handleMessageSend = () => {
+    console.log('canled');
+    socket.on('send_message', () => {
+      console.log(socket.id, socket.connected);
+    });
+    socket.emit('send_message', {
+      message: message,
+    });
+  };
+
+  console.log(message);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Indian Meet Chatting</h1>
+      Message :
+      <input type="text" value={message} onChange={handleMessageChange} />
+      <button onClick={handleMessageSend}>Send</button>
     </div>
   );
-}
+};
 
 export default App;
